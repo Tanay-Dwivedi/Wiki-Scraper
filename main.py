2902585b33a3a48c5de5916dddc14cddde63912e
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-01-23 15:02:00 +0530

    streamlit added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# for the streamlit 
+# for the streamlit
+
+import streamlit as st
+from scrap_wiki import text_scrap
+
+
+url = st.text_input("Enter the Wikipedia page URL")
+
+submit_btn = st.button("Enter")
+
+if submit_btn:
+    text = text_scrap(url)
+    st.write(text)
--- scrap_wiki.py
+++ scrap_wiki.py
@@ -1,28 +1,25 @@
 import requests
-import nltk
 from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
 import re
 
-url = input("Enter the Wiki url: ")
-
-res = requests.get(url)
-
-wiki_text = BeautifulSoup(res.text, "html.parser")
-
-text = ""
-for para in wiki_text.select("p"):
-    text += para.getText()
+# url = input("Enter the Wiki url: ")
 
 
-text = re.sub(r"\[[0-9]*\]", " ", text)
-text = re.sub(r"\s+", " ", text)
-text = text.lower()
-text = re.sub(r"\d", " ", text)
-text = re.sub(r"\s+", " ", text)
+def text_scrap(url):
+    res = requests.get(url)
 
-sentences = nltk.sent_tokenize(text)
+    wiki_text = BeautifulSoup(res.text, "html.parser")
 
-sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+    text = ""
+    for para in wiki_text.select("p"):
+        text += para.getText() + "\n"
 
-print(sentences)
+    text = re.sub(r"\[[0-9]*\]", " ", text)
+    text = re.sub(r"\s+", " ", text)
+    text = text.lower()
+    text = re.sub(r"\[citation needed\]", " ", text)
+    text = re.sub(r"`", "", text)
+    text = text.replace("[", "(")
+    text = text.replace("]", ")")
+    return text
+
