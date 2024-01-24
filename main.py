48fb33b75dd5bcf2431563544b9943a889b53a19
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-01-24 23:52:34 +0530

    added selectbox

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import wikipedia as wk
 with sl.form("Wiki Search"):
     url = sl.text_input("Enter the Wikipedia page URL")
     url = url.split("/")[-1]
-    text, summary, images, refrences = sl.columns(4)
-    text_submit_btn = text.form_submit_button("Content")
-    summary_submit_btn = summary.form_submit_button("Summary")
-    images_submit_btn = images.form_submit_button("Images")
-    refrences_submit_btn = refrences.form_submit_button("Refrences")
+    val = sl.selectbox(
+        "Search for", options=["Content", "Summary", "Images", "Refrences", "All data"]
+    )
+    submit_btn = sl.form_submit_button("Search")
 
 sl.write("##")
 
@@ -38,31 +37,54 @@ def wiki_refrences(url):
     return ref_urls
 
 
-if text_submit_btn:
-    sl.write("## Wikipedia Text:")
-    sl.write("##")
-    sl.write(wiki_content(url))
-
-if summary_submit_btn:
-    sl.write("## Wikipedia Summary:")
-    sl.write("##")
-    sl.write(wiki_summary(url))
-
-if images_submit_btn:
-    sl.write("## Wikipedia Summary:")
-    sl.write("##")
-    img_list = wiki_images(url)
-    img_len = len(img_list)
-    for i in range(0, img_len):
-        sl.image(
-            img_list[i],
-            caption=img_list[i].split("/")[-1].split(".")[0],
-            width=200,
-            use_column_width="always",
-        )
-
-if refrences_submit_btn:
-    sl.write("## Wikipedia refrences:")
-    sl.write("##")
-    for i, refs in enumerate(wiki_refrences(url), 1):
-        sl.write(i, " - ", refs)
+if submit_btn:
+    if val == "Content":
+        sl.write("## Wikipedia Text:")
+        sl.write("##")
+        sl.write(wiki_content(url))
+    elif val == "Summary":
+        sl.write("## Wikipedia Summary:")
+        sl.write("##")
+        sl.write(wiki_summary(url))
+    elif val == "Images":
+        sl.write("## Wikipedia Images:")
+        sl.write("##")
+        img_list = wiki_images(url)
+        img_len = len(img_list)
+        for i in range(0, img_len):
+            sl.image(
+                img_list[i],
+                caption=img_list[i].split("/")[-1].split(".")[0],
+                width=200,
+                use_column_width="always",
+            )
+    elif val == "Refrences":
+        sl.write("## Wikipedia refrences:")
+        sl.write("##")
+        for i, refs in enumerate(wiki_refrences(url), 1):
+            sl.write(i, " - ", refs)
+    else:
+        sl.write("## Wikipedia Text:")
+        sl.write("##")
+        sl.write(wiki_content(url))
+        sl.write("##")
+        sl.write("## Wikipedia Summary:")
+        sl.write("##")
+        sl.write(wiki_summary(url))
+        sl.write("##")
+        sl.write("## Wikipedia Images:")
+        sl.write("##")
+        img_list = wiki_images(url)
+        img_len = len(img_list)
+        for i in range(0, img_len):
+            sl.image(
+                img_list[i],
+                caption=img_list[i].split("/")[-1].split(".")[0],
+                width=200,
+                use_column_width="always",
+            )
+        sl.write("##")
+        sl.write("## Wikipedia refrences:")
+        sl.write("##")
+        for i, refs in enumerate(wiki_refrences(url), 1):
+            sl.write(i, " - ", refs)
